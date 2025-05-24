f4a8a62cac5032a28e5b1df0c7dc045bc24991e2
Umbrason <Umbrason@Outlook.com>
2025-05-24 16:26:53 +0200

    Update to RenderGraphAPI

--- Scripts/HitFlashRenderFeature.cs
+++ Scripts/HitFlashRenderFeature.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.RenderGraphModule.Util;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.U2D;
+
 
 public class HitFlashRenderFeature : ScriptableRendererFeature
 {
@@ -86,6 +89,7 @@ public class HitFlashRenderFeature : ScriptableRendererFeature
     private class HitFlashSettings
     {
         public Color32 color;
+        public Material hitflashBlitMaterial;
         public RenderPassEvent renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
         public float duration;
     }
@@ -108,66 +112,93 @@ public class HitFlashRenderFeature : ScriptableRendererFeature
     {
         private HitFlashSettings settings;
         private Material hitflashBlitMaterial;
+        RenderTextureDescriptor hitFlashMaskTextureDescriptor;
         public HitFlashRenderPass(HitFlashSettings settings)
         {
             this.settings = settings;
-            hitflashBlitMaterial = new Material(Shader.Find("Hitflash/Blit"));
-            hitflashBlitMaterial.SetColor("color", settings.color);
+            hitflashBlitMaterial = settings.hitflashBlitMaterial ?? new Material(Shader.Find("Hitflash/Blit"));
+            hitflashBlitMaterial?.SetColor("color", settings.color);
+            hitFlashMaskTextureDescriptor = new(Screen.width, Screen.height, RenderTextureFormat.Default, 0);
         }
 
         public void Dispose()
         {
             DestroyImmediate(hitflashBlitMaterial);
-            FlashMaskRT?.Release();
         }
 
-        private RTHandle FlashMaskRT;
-        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        public class HitflashPassData
         {
-            var camTargetDesc = renderingData.cameraData.cameraTargetDescriptor;
-            if (FlashMaskRT == null || FlashMaskRT.rt.width != camTargetDesc.width || FlashMaskRT.rt.height != camTargetDesc.height)
-            {
-                if (FlashMaskRT != null) FlashMaskRT.Release();
-                FlashMaskRT = RTHandles.Alloc(width: camTargetDesc.width, height: camTargetDesc.height, name: "hitFlashMask", colorFormat: UnityEngine.Experimental.Rendering.GraphicsFormat.R8G8B8A8_UNorm);
-            }
+            public HitflashInfo hitflashInfo;
+            public TextureHandle hitFlashMaskTextureHandle;
+        }
+
+        public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+        {
+            var resourceData = frameData.Get<UniversalResourceData>();
+            var cameraData = frameData.Get<UniversalCameraData>();
+
+            if (resourceData.isActiveTargetBackBuffer) return;
+
+            hitFlashMaskTextureDescriptor.width = cameraData.cameraTargetDescriptor.width;
+            hitFlashMaskTextureDescriptor.height = cameraData.cameraTargetDescriptor.height;
+            hitFlashMaskTextureDescriptor.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R8G8B8A8_UNorm;
+            hitFlashMaskTextureDescriptor.depthBufferBits = 0;
+
+            var srcCamColorHandle = resourceData.activeColorTexture;
+            var srcCamDepthHandle = resourceData.activeDepthTexture;
+            var hitFlashMaskTextureHandle = UniversalRenderer.CreateRenderGraphTexture(renderGraph, hitFlashMaskTextureDescriptor, "Hitflash_Mask", false);
+
+            // This check is to avoid an error from the material preview in the scene
+            if (!(srcCamColorHandle.IsValid() && srcCamDepthHandle.IsValid() && hitFlashMaskTextureHandle.IsValid()))
+                return;
 
             //remove old entries
             var minValidStartTime = Time.time - settings.duration;
             while (activeHitFlashes.Count > 0 && activeHitFlashes[0].startTime < minValidStartTime)
                 activeHitFlashes.RemoveAt(0);
-
-            //render active entries
-            var cmd = CommandBufferPool.Get();
-            cmd.name = "DrawHitFlash";
-            for (int i = 0; i < activeHitFlashes.Count; i++)
+            if (hitflashBlitMaterial == null) return;
+            var activeFlashes = activeHitFlashes;
+            for (int i = 0; i < activeFlashes.Count; i++)
             {
-                context.ExecuteCommandBuffer(cmd);
-                cmd.Clear();
-                var hitflash = activeHitFlashes[i];
-                if (hitflashBlitMaterial == null) break;
-                hitflashBlitMaterial.SetFloat("t", (hitflash.startTime - Time.time) / settings.duration);
-                for (int r = 0; r < hitflash.renderTargets.Length; r++)
+                var hitflash = activeFlashes[i];
+                using (var renderPass = renderGraph.AddRasterRenderPass<HitflashPassData>("Hitflash-Render", out var data))
                 {
-                    var renderTarget = hitflash.renderTargets[r];
-                    if (renderTarget.Renderer == null) continue;
-                    if (renderTarget.Mesh == null) continue;
-                    var mesh = renderTarget.Mesh;
-                    var materials = renderTarget.Materials;
-                    for (int m = 0; m < mesh.subMeshCount; m++)
-                    {
-                        var shaderPass = materials[m].FindPass("ForwardLit");
-                        cmd.SetRenderTarget(FlashMaskRT, depth: renderingData.cameraData.renderer.cameraDepthTargetHandle);
-                        cmd.ClearRenderTarget(RTClearFlags.Color, new Color(0, 0, 0, 0), 1f, 0);
-                        cmd.DrawRenderer(renderTarget.Renderer, materials[m], m, shaderPass);
-                    }
-                    cmd.Blit(FlashMaskRT, renderingData.cameraData.renderer.cameraColorTargetHandle, mat: hitflashBlitMaterial);
-                    context.ExecuteCommandBuffer(cmd);
-                    cmd.Clear();
+                    data.hitflashInfo = hitflash;
+                    data.hitFlashMaskTextureHandle = hitFlashMaskTextureHandle;
+                    renderPass.SetRenderAttachment(hitFlashMaskTextureHandle, 0);
+                    renderPass.SetRenderAttachmentDepth(srcCamDepthHandle, 0);
+                    renderPass.SetRenderFunc<HitflashPassData>(ExecuteHitflashRenderPass);
+                }
+                var blitPropertyBlock = new MaterialPropertyBlock();
+                blitPropertyBlock.SetFloat("t", (hitflash.startTime - Time.time) / settings.duration);
+                RenderGraphUtils.BlitMaterialParameters parameters = new(hitFlashMaskTextureHandle, srcCamColorHandle, hitflashBlitMaterial, 0)
+                {
+                    sourceTexturePropertyID = Shader.PropertyToID("_MainTex"),
+                    propertyBlock = blitPropertyBlock,
+                    geometry = RenderGraphUtils.FullScreenGeometryType.ProceduralTriangle
+                };
+                renderGraph.AddBlitPass(parameters);
+            }
+        }
+
+        private void ExecuteHitflashRenderPass(HitflashPassData data, RasterGraphContext context)
+        {
+            //render active entries
+            var cmd = context.cmd;
+            cmd.ClearRenderTarget(RTClearFlags.Color, new Color(0, 0, 0, 0), 1f, 0);
+            for (int r = 0; r < data.hitflashInfo.renderTargets.Length; r++)
+            {
+                var renderTarget = data.hitflashInfo.renderTargets[r];
+                if (renderTarget.Renderer == null) continue;
+                if (renderTarget.Mesh == null) continue;
+                var mesh = renderTarget.Mesh;
+                var materials = renderTarget.Materials;
+                for (int m = 0; m < mesh.subMeshCount; m++)
+                {
+                    var shaderPass = materials[m].FindPass("ForwardLit");
+                    cmd.DrawRenderer(renderTarget.Renderer, materials[m], m, shaderPass);
                 }
             }
-            context.ExecuteCommandBuffer(cmd);
-            cmd.Clear();
-            CommandBufferPool.Release(cmd);
         }
     }
 }
